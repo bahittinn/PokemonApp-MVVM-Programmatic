cf8786ed25ff616af45fda28fe6c546ab6ff2674
bahittinn <bahittin84@gmail.com>
2023-10-16 23:28:57 +0300

    Initial Commit

---
+++ PokemonApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonApp
+//
+//  Created by Bahittin on 16.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
